78b8340f5ede76089253486d2511b0c4723d51ca
Buterbrod-iku <melkov750@gmail.com>
2022-11-27 15:50:21 +0700

    first

---
+++ LR_1/prog_2/prog_2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int first, second, res;
+    cout << "РАЗНОСТЬ" << endl << "Первое число: " << endl;
+    cin >> first;
+    cout << "Второе число: " << endl;
+    cin >> second;
+    res = first - second;
+    cout << "Результат: " << res;
+    return 0;
+}
